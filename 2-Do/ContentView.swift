4c690bf1478ff0ea5656ea2d2ce959cb1b68375a
Oscar <oscarleigh@icloud.com>
2026-06-12 23:20:04 +0100

    Initial Commit

---
+++ 2-Do/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2-Do
+//
+//  Created by Oscar Leigh on 12/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worlda!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2-Do/__DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  __DoApp.swift
+//  2-Do
+//
+//  Created by Oscar Leigh on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct __DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
